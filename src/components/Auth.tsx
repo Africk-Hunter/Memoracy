59af268564a668fba4c2d906d2365044bc22b3e4
Africk-Hunter <hunterafrick@gmail.com>
2025-03-18 14:09:37 -0700

    Added Toast Messages
    - Added toast message displaying what went wrong (or right) in user sign in/sign up
    - Removed manifest.json

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -1,33 +1,36 @@
 import React, { useState } from "react";
 import { auth } from "../firebaseConfig";
 import { signInWithEmailAndPassword, createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import { useNavigate } from "react-router-dom";
 
 const Auth: React.FC = () => {
 
     const [option, setOption] = useState("Sign In");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const [pwError, setPwError] = useState("");
+    const [messageBoxMessage, setMessageBoxMessage] = useState("");
+    const [messageType, setMessageType] = useState("");
 
+    function displayMessage(message: string, type: string) {
+        setMessageBoxMessage(message);
+        setMessageType(type);
+    }
 
     function checkPassword(password: string) {
         if (password.length < 6) {
-            setPwError("Password must be at least 6 characters long");
-            console.log(pwError)
+            displayMessage("Password must be at least 6 characters long", "bad");
+            console.log(messageBoxMessage)
             return false;
         } else if (password.length > 20) {
-            setPwError("Password must be less than 20 characters long");
-            console.log(pwError)
+            displayMessage("Password must be less than 20 characters long", "bad");
+            console.log(messageBoxMessage)
             return false;
         }
-        setPwError("");
+        setMessageBoxMessage("");
         return true;
 
     }
 
     interface AuthFormEvent extends React.FormEvent<HTMLFormElement> { }
-
     function defaultSignUp(e: AuthFormEvent): void {
         e.preventDefault();
         if (checkPassword(password)) {
@@ -42,6 +45,9 @@ const Auth: React.FC = () => {
             .then((userCredential) => {
                 const user = userCredential.user;
                 console.log(user);
+                displayMessage("Successfully created account!", "good");
+                setEmail("");
+                setPassword("");
             })
             .catch((error) => {
                 const errorCode = error.code;
@@ -54,9 +60,10 @@ const Auth: React.FC = () => {
         signInWithEmailAndPassword(auth, email, password)
             .then((userCredential) => {
                 const user = userCredential.user;
-                window.location.href = '/'; /* Place holder */
+                window.location.href = '/'; /* Placeholder */
             })
             .catch((error) => {
+                displayMessage('Invalid email or password. Please try again', 'bad');
                 console.log('Invalid email or password. Please try again')
                 const errorCode = error.code;
                 const errorMessage = error.message;
@@ -65,6 +72,8 @@ const Auth: React.FC = () => {
 
     return (
         <div className="auth">
+            <MessageBox messageBoxMessage={messageBoxMessage} goodOrBad={messageType} />
+
             <section className="shadowAndBorder loginBox ">
                 <section className="text">
                     <h1 className="loginTagline">{option === 'Sign In' ? 'Welcome Back' : 'Let\'s get started!'}</h1>
@@ -76,7 +85,7 @@ const Auth: React.FC = () => {
                 </section>
                 <form className="loginForm" onSubmit={option === 'Sign In' ? (e) => handleSignIn(e) : (e) => defaultSignUp(e)}>
                     <input type="email" placeholder="email@domain.com" className="loginInput" onChange={(e) => setEmail(e.target.value)} />
-                    <input type="password" placeholder="Password" className="loginInput" onChange={(e) => setPassword(e.target.value)} />
+                    <input type="password" placeholder="Password" autoComplete="off" className="loginInput" onChange={(e) => setPassword(e.target.value)} />
                     <button className="loginButton email shadowAndBorder">Continue with Email</button>
                 </form>
                 <hr className="loginDivider" />
@@ -100,4 +109,18 @@ function OptionType({ typeText, option, setOption }: OptionTypeProps) {
         <button className={option === typeText ? 'optionButton active' : 'optionButton'} onClick={() => setOption(typeText)}>{typeText}</button>
     )
 }
+
+interface MessageBoxProps {
+    messageBoxMessage: string;
+    goodOrBad: string;
+}
+
+function MessageBox({ messageBoxMessage, goodOrBad }: MessageBoxProps) {
+    return (
+        <section className={`messageBox shadowAndBorder ${goodOrBad}`}>
+            <p className="messageBoxMessage">{messageBoxMessage}</p>
+        </section>
+    )
+}
+
 export default Auth;
